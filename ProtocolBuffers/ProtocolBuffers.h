a19abce253059359b7197c33cd4715666bad024d
Nguyen Dung <dungnt@vivas.vn>
2017-03-10 16:32:40 +0700

    add souce

---
+++ ProtocolBuffers/ProtocolBuffers.h
@@ -0,0 +1,17 @@
+//
+//  ProtocolBuffers.h
+//  ProtocolBuffers
+//
+//  Created by Nguyen Dung on 3/10/17.
+//  Copyright © 2017 Nguyen Dung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProtocolBuffers.
+FOUNDATION_EXPORT double ProtocolBuffersVersionNumber;
+
+//! Project version string for ProtocolBuffers.
+FOUNDATION_EXPORT const unsigned char ProtocolBuffersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocolBuffers/PublicHeader.h>
